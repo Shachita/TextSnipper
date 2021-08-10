ecd6e887a86fb04bd1a54e38f4b5a8a6f9d9aee0
Shachita <shachitajains@gmail.com>
2021-08-10 19:19:30 +0530

    Added Procfile and collected static files for deployment

---
+++ Procfile
@@ -0,0 +1,3 @@
+web: gunicorn TextSnippets.wsgi 
+
+
--- TextSnippets/settings.py
+++ TextSnippets/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR/'static'
